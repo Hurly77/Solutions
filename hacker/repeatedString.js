d98c850facf49e418a766c2f0dc92489aa3fb31f
Hurly77 <cameron.leverett@yahoo.com>
2021-02-25 12:01:34 -0800

    added new test

---
+++ hacker/repeatedString.js
@@ -0,0 +1,33 @@
+const aF = require('../utils')
+const a = new aF('abc', 100)
+let array = a.createString()
+
+const repeatedString = (s, n) => {
+  let a = 0
+  let na = 0;
+  let end = n % s.lengthr
+  for (let i = 0; i < s.length; i++) {
+    s[i] === 'a' ?  a += 1 : na += 1;
+  }
+  
+  if(n % s.length === 0 ){
+    return n/s.length * a
+  }
+  if(n % s.length != 0 && s.length > n){
+    let left = Math.floor(n/s.length * a);
+    let j = 0;
+    for (let i = 0; i < s.slice(0, end).length; i++) {
+        s[i] === 'a' ? j += 1 : null
+      }
+      return j + left;
+    }else{
+      let j = 0
+      for(let i = 0; i < s.slice(0, n); i++){
+        s[i] === 'a' ? j += 1 : null; 
+      }
+      return j
+    }
+}
+//  aab
+882787
+console.log(repeatedString('ababa', 3))
