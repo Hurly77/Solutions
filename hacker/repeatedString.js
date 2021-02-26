a5e31840fbf68da782ee4ed15d6c91112fa736a2
Hurly77 <cameron.leverett@yahoo.com>
2021-02-25 23:29:17 -0800

    still woring on a test case

--- hacker/repeatedString.js
+++ hacker/repeatedString.js
@@ -5,7 +5,7 @@ let array = a.createString()
 const repeatedString = (s, n) => {
   let a = 0
   let na = 0;
-  let end = n % s.lengthr
+  let end = n % s.length
   for (let i = 0; i < s.length; i++) {
     s[i] === 'a' ?  a += 1 : na += 1;
   }
@@ -13,13 +13,14 @@ const repeatedString = (s, n) => {
   if(n % s.length === 0 ){
     return n/s.length * a
   }
-  if(n % s.length != 0 && s.length > n){
-    let left = Math.floor(n/s.length * a);
+  if(n % s.length != 0 && s.length < n){
+    let net = Math.floor(n/s.length * a);
     let j = 0;
+    console.log(n % s.length, n/s.length * a)
     for (let i = 0; i < s.slice(0, end).length; i++) {
         s[i] === 'a' ? j += 1 : null
       }
-      return j + left;
+      return j + net;
     }else{
       let j = 0
       for(let i = 0; i < s.slice(0, n); i++){
@@ -28,6 +29,4 @@ const repeatedString = (s, n) => {
       return j
     }
 }
-//  aab
-882787
-console.log(repeatedString('ababa', 3))
+console.log(repeatedString('epsxyyflvrrrxzvnoenvpegvuonodjoxfwdmcvwctmekpsnamchznsoxaklzjgrqruyzavshfbmuhdwwmpbkwcuomqhiyvuztwvq', 549382313570))
