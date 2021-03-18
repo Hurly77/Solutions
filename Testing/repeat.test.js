d7cc52d658d947a664cc94dbf10b9787fa3e4d6c
Hurly77 <cameron.leverett@yahoo.com>
2021-03-18 13:18:49 -0700

    added testing for Big-o

--- Testing/repeat.test.js
+++
@@ -1,6 +0,0 @@
-const repeatedString = require('../hacker/repeatedString');
-const num = 10
-const s = 'abc'
-test('Calculate Number of repeted characters', () => {
-  expect(repeatedString(s, num).toBe(4))
-});
