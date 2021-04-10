610d1196c5e8433406a4ca83be351e27cc7b2257
Cameron Leverett <camrbo@gmail.com>
2021-04-10 15:20:06 -0700

    updating

--- hacker/RansomNote.js
+++ hacker/RansomNote.js
@@ -1,19 +1,32 @@
-const ransomNote = (mag, note) => {
-  let map = {};
-  for (let i = 0; i < note.length; i++) {
-    (map[note[i]] += 1) || (map[note[i]] = 1);
-  }
+// const ransomNote = (mag, note) => {
+//   let map = {};
+//   for (let i = 0; i < note.length; i++) {
+//     (map[note[i]] += 1) || (map[note[i]] = 1);
+//   }
 
-  for (let j = 0; j < mag.length; j++) {
-    if (map[mag[j]]) {
-      map[mag[j]] -= 1;
-    }
+//   for (let j = 0; j < mag.length; j++) {
+//     if (map[mag[j]]) {
+//       map[mag[j]] -= 1;
+//     }
+//   }
+//   for (key in map) {
+//     if (map[key] > 0) {
+//       console.log('No');
+//       return;
+//     }
+//   }
+//   console.log('Yes');
+// };
+const rad = require('@hurly77/rad-arrays')
+let array1 = rad.array('string', 10000000)
+
+const sortArray = (arr) => {
+  const item1 = arr[0]
+  const item2 = arr[1]
+
+  for(let i = 0; i < arr.length; i++){
+    
   }
-  for (key in map) {
-    if (map[key] > 0) {
-      console.log('No');
-      return;
-    }
-  }
-  console.log('Yes');
-};
+}
+
+console.log()
